cc73de0badc664ed424fd7b46a107825257e8692
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-04-28 17:09:19 -0500

    Update Algorithm

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 for head in range(10):
     for track in range(1, 10):
-        if head == 0:
-            print("{:02d}".format(track), end=", ")
-        elif head > 0 and track < (10 - head):
-            if head < 8:
-                print("{:02d}".format((track + head) + (head * 10)), end=", ")
-            else:
-                print("{:02d}".format((track + head) + (head * 10)))
+        if track < (10 - head):
+            print("{:02d}".format((track + head) + (head * 10)),
+                  end=", " if head < 8 else '\n')
