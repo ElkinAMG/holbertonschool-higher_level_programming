9a91094dd07de0c7c9416d724de9eeff4a1f7e5f
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-04-29 10:08:44 -0500

    Third Advanced Added

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import dis
+
 
 def magic_calculation(a, b, c):
 
@@ -8,5 +8,3 @@ def magic_calculation(a, b, c):
     if c > b:
         return a + b
     return a * b - c
-
-dis.dis(magic_calculation)
