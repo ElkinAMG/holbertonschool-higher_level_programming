da2248f20fe01afce73063d82847ba75ab6ea707
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-04-30 19:25:10 -0500

    Last Advanced

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,24 +5,25 @@ if __name__ == "__main__":
 
     argc = len(argv[1:])
 
-    if argc == 3:
-        number_one, number_two = int(argv[1]), int(argv[3])
-        operators = ['+', 'add(number_one, number_two)',
-                     '-', 'sub(number_one, number_two)',
-                     'x', 'mul(number_one, number_two)',
-                     '/', 'div(number_one, number_two)']
-
-        for index, tripper in enumerate(operators):
-            if argv[2] == operators[index]:
-                print("{:d} {:s} {:d} = {:d}".format(
-                    number_one,
-                    operators[index],
-                    number_two,
-                    eval(operators[index + 1])))
-                break
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-    else:
+    if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a, b = int(argv[1]), int(argv[3])
+    sign = argv[2]
+    operators = ['+', 'add(a, b)',
+                 '-', 'sub(a, b)',
+                 '*', 'mul(a, b)',
+                 '/', 'div(a, b)']
+
+    for index, tripper in enumerate(operators):
+        if sign == operators[index]:
+            print("{:d} {:s} {:d} = {:d}".format(
+                a,
+                sign,
+                b,
+                eval(operators[index + 1])))
+            break
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
