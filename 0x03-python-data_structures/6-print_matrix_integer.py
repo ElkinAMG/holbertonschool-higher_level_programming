c304d29514c3d9f14940048a0eb472352e865af7
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-05-05 13:22:06 -0500

    Fixed Errors

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,6 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for y in range(len(x)):
-            print("{:d}".format(x[y]), end=" " if
-                  y != (len(x) - 1) else "\n")
+            cend = " " if y != (len(x) - 1) else ""
+            print("{:d}".format(x[y]), end=cend)
+        print()
