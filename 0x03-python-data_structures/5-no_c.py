e905fa6623922bc8456384aa118dbdbe1b712a17
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-05-06 14:45:08 -0500

    Simple Code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,5 @@
 
 
 def no_c(my_string):
-    new_string = ''.join([my_string[i]
-                          for i in range(len(my_string))
-                          if my_string[i] not in "Cc"])
+    new_string = ''.join([char for char in my_string if char not in "cC"])
     return (new_string)
