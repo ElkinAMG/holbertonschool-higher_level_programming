e0df23404825732e4cc4dd3af6ade3ea3e138723
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-05-18 12:20:34 -0500

    Fixing 1st and 4th

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,7 +6,7 @@ def safe_print_integer(value):
 
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         bool = False
 
     return (bool)
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,19 +3,28 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     n_list = []
-    t_error = {
-        ZeroDivisionError: 'division by 0',
-        TypeError: 'wrong type',
-        IndexError: 'out of range'
-    }
+    #    t_error = {
+    #        ZeroDivisionError: 'division by 0',
+    #        TypeError: 'wrong type',
+    #        IndexError: 'out of range'
+    #    }
 
     for i in range(list_length):
         try:
             n_list.append(my_list_1[i] / my_list_2[i])
-        except (ZeroDivisionError, TypeError, IndexError) as error:
-            for x in t_error.keys():
-                if type(error) == x:
-                    print(t_error.get(x))
-                    n_list.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            n_list.append(0)
+        except TypeError:
+            print("wrong type")
+            n_list.append(0)
+        except IndexError:
+            print("out of range")
+            n_list.append(0)
+            #       except (ZeroDivisionError, TypeError, IndexError) as error:
+            #           for x in t_error.keys():
+            #               if type(error) == x:
+            #                   print(t_error.get(x))
+            #                   n_list.append(0)
 
     return (n_list)
