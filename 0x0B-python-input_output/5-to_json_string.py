7eee2f558de7bbced806f6c636a76dc060b10e0b
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-06-03 18:42:16 -0500

    5th Up

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+'''
+
+This module has `to_json_string`.
+
+'''
+
+
+def to_json_string(my_obj):
+    '''
+    Returns the JSON representation of `my_obj`.
+    '''
+    return (json.dumps(my_obj))
