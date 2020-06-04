f5e7f5c658d4088288d03b37287c1dbc0639bfda
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-06-03 19:21:46 -0500

    8th Up

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+
+This module has `load_from_json_file`.
+
+'''
+import json
+
+
+def load_from_json_file(filename):
+    '''
+    Creates an Object from `filename`.
+    '''
+
+    with open(filename) as f:
+        return(json.loads(filename))
