09c57cfe04c5ebab4584581c98dc899bd86039f4
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-06-03 21:41:58 -0500

    12 Up

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+
+This module has `class_to_join`.
+
+'''
+
+
+def class_to_json(obj):
+    '''
+    Returns the dictionary description with simple data structure
+    '''
+
+    return (obj.__dict__)
---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+
+This modules has `Student` class.
+
+'''
+
+
+class Student:
+    '''
+    Defines a student.
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''
+        Initial constructor.
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''
+        Retrieves a dictionary representation of a `Student`
+        '''
+        return (self.__dict__)
