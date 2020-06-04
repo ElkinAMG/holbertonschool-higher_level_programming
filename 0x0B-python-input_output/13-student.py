98b3dda90471c9a59ef810fd66dbcb17129e4d14
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-06-03 22:10:14 -0500

    13th Up

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+'''
+
+This modules has `Student` class.
+
+'''
+
+
+class Student:
+    '''
+    Defines a student.
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''
+        Initial constructor.
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        Retrieves a dictionary representation of a `Student`
+        '''
+        attr = {}
+        if attrs is not None:
+            for i in attrs:
+                if hasattr(self, i):
+                    attr[i] = getattr(self, i)
+        else:
+            return (self.__dict__)
+        return (attr)
+
+    def relaod_from_json(self, json):
+        '''
+        Replaces all attributes of `Student`.
+        '''
+
+        for k, v in json.items():
+            if hasattr(self, k):
+                setattr(self, k, v)
